d3389b01296f3a9c324c2831f7a6477cac48e152
Leo Jeong <80350771+33tm@users.noreply.github.com>
2025-10-12 16:59:27 -0700

    static export

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,18 @@
 import type { NextConfig } from "next"
 
-const links = [
-    ["/conduct", "https://docs.google.com/document/d/e/2PACX-1vRV7AHc6eftjfLNjDAVeSo33rkgmD9E37tj9sFDsEAnKrLxFzX-KFZY6ajFEZxbQZmO_l_3KNaR0Wm3/pub"],
-    ["/devpost", "https://gunnhacks12.devpost.com"]
-]
+// TODO redeploy w/ server or create individual routes for each redirect
+// const links = [
+//     ["/conduct", "https://docs.google.com/document/d/e/2PACX-1vRV7AHc6eftjfLNjDAVeSo33rkgmD9E37tj9sFDsEAnKrLxFzX-KFZY6ajFEZxbQZmO_l_3KNaR0Wm3/pub"],
+//     ["/devpost", "https://gunnhacks12.devpost.com"]
+// ]
 
 export default {
-    redirects: async () => links.map(([source, destination]) => ({
-        source,
-        destination,
-        permanent: false
-    })),
+    // redirects: async () => links.map(([source, destination]) => ({
+    //     source,
+    //     destination,
+    //     permanent: false
+    // })),
     // github pages static export
-    images: { unoptimized: true }
+    images: { unoptimized: true },
+    output: "export"
 } satisfies NextConfig
