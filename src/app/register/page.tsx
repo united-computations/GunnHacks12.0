a98cd5058940afe53c4cdc4dad861b589da8b0de
Leo Jeong <80350771+33tm@users.noreply.github.com>
2026-02-09 18:20:17 -0800

    /register

---
+++ src/app/register/page.tsx
@@ -0,0 +1,3 @@
+// this is terrible... use redirects in /next.config.ts if you're able to ssr
+
+export default () => <meta httpEquiv="Refresh" content="0; url=https://forms.gle/tbiXpqE4zxFjp6dg6" />
